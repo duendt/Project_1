f3469f1a58482d13a1b76832499ad4afeb327428
Quanpnph53348 <quanpnph53348@gmail.com>
2025-04-22 22:41:58 +0700

    vong quay

---
+++ app/Controller/ClientController/VoucherController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\ClientController;
+
+class VoucherController{
+    public function __construct()
+    {
+        // Constructor code here
+    }
+
+    public function index()
+    {
+        // Code to display the list of vouchers
+    }
+
+    public function create()
+    {
+        // Code to create a new voucher
+    }
+
+    public function edit($id)
+    {
+        // Code to edit an existing voucher
+    }
+
+    public function delete($id)
+    {
+        // Code to delete a voucher
+    }
+}
